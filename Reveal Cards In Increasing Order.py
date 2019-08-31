baa27cc9762b2fb22be9c25cb901e6912df89b18
Tejsvi Juj <30386504+TejsviJ@users.noreply.github.com>
2019-08-31 17:11:12 +0530

    Create Reveal Cards In Increasing Order.py

---
+++ Reveal Cards In Increasing Order.py
@@ -0,0 +1,16 @@
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        N = len(deck)
+        index = collections.deque(range(N))
+        print(index)
+        ans = [None] * N
+
+        for card in sorted(deck):
+          
+            
+            ans[index.popleft()] = card
+            if index:
+                index.append(index.popleft())
+
+        return ans
+        
